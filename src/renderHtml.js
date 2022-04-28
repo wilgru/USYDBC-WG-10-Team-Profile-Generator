2ceac5755922ba50756fcf5706e2fa22ff570325
wilgru <william.gruszka@gmail.com>
2022-04-28 16:38:16 +1000

    updated card templates

--- src/renderHtml.js
+++ src/renderHtml.js
@@ -1,4 +1,4 @@
-//
+// return full HTML page
 function renderHtml (data) {
     const htmlContent = 
    `<!DOCTYPE html>
@@ -8,6 +8,18 @@ function renderHtml (data) {
        <meta http-equiv="X-UA-Compatible" content="IE=edge">
        <meta name="viewport" content="width=device-width, initial-scale=1.0">
        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+       <style>
+            .employee-card {
+                min-width: 16rem; 
+                padding: 0; 
+                flex-grow:0; 
+                opacity: 0; 
+                transition: 400ms;
+            }
+            .employee-card:hover {
+                box-shadow: 5px 5px 5px grey;
+            }
+       </style>
        <title>My Team Profile</title>
    </head>
    <body>
@@ -23,6 +35,21 @@ function renderHtml (data) {
                </div>
            </div>
        </main>
+
+       <script>
+            const cards = document.querySelectorAll(".employee-card")
+            const numCards = cards.length
+            let index = 0
+
+            const cardsDelay = setInterval(() => {
+                if (index === numCards-1) {
+                    clearInterval(cardsDelay)
+                } 
+
+                cards[index].style.opacity = 1
+                index++
+            }, 100)
+        </script>
    </body>
    </html>
    `
@@ -53,10 +80,10 @@ function generateCards (data) {
 // create new html card for a manager
 function createManagerCard (manager) {
     const template = 
-    `<div class="card text-dark bg-light m-3 col" style="min-width: 16rem; padding: 0;  flex-grow:0;">
+    `<div class="card text-dark bg-light m-3 col employee-card">
         <div class="card-header text-light bg-primary">
             <h3 class="card-title">${manager.name}</h3>
-            <p style="margin: 0;">Manager</p>
+            <p style="margin: 0;">👔 Manager</p>
         </div>
         <div class="card-body">
             <p class="card-text">ID: ${manager.id}</p>
@@ -71,15 +98,15 @@ function createManagerCard (manager) {
 // create new html card for an engineer
 function createEngineerCard (engineer) {
     const template = 
-    `<div class="card text-dark bg-light m-3 col" style="min-width: 16rem; padding: 0;  flex-grow:0;">
+    `<div class="card text-dark bg-light m-3 col employee-card">
         <div class="card-header text-light bg-success">
             <h3 class="card-title">${engineer.name}</h3>
-            <p style="margin: 0;">Engineer</p>
+            <p style="margin: 0;">🛠 Engineer</p>
         </div>
         <div class="card-body">
             <p class="card-text">ID: ${engineer.id}</p>
             <p class="card-text">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
-            <p class="card-text">Github: <a href="https://gitub.com/${engineer.github}">${engineer.github}</a></p>
+            <p class="card-text">Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
         </div>
     </div>`
 
@@ -89,10 +116,10 @@ function createEngineerCard (engineer) {
 // create new html card for an intern
 function createInternCard (intern) {
     const template = 
-    `<div class="card text-dark bg-light m-3 col" style="min-width: 16rem; padding: 0;  flex-grow:0;">
+    `<div class="card text-dark bg-light m-3 col employee-card">
         <div class="card-header text-light bg-warning">
             <h3 class="card-title">${intern.name}</h3>
-            <p style="margin: 0;">Intern</p>
+            <p style="margin: 0;">📚 Intern</p>
         </div>
         <div class="card-body">
             <p class="card-text">ID: ${intern.id}</p>
