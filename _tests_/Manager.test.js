35c51b8a2eb1467cd085a97240c33f755f64969d
wilgru <william.gruszka@gmail.com>
2022-05-03 08:41:20 +1000

    added test for getOfficeNum()

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -3,10 +3,19 @@ const Manager = require("../lib/Manager")
 describe("Emplyee", () => {
     describe("getRole", () => {
         it("Should return the string 'Manager'", () => {
-            const newManager = new Manager("John", "01", "john.doe@gmail.com", "johndoe1", "room_1");
+            const newManager = new Manager("John", "01", "john.doe@gmail.com", "room_1", "johndoe1");
             const role = newManager.getRole();
 
             expect(role).toEqual("Manager");
+        })
+    })
+
+    describe("getOfficeNum", () => {
+        it("Should return the string 'room_1'", () => {
+            const newManager = new Manager("John", "01", "john.doe@gmail.com", "room_1", "johndoe1");
+            const officeNum = newManager.getOfficeNum();
+
+            expect(officeNum).toEqual("room_1");
         })
     })
 })
